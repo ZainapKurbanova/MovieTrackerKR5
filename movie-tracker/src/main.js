e70958326ddc3b43437923b8df4e4e64ccb3104b
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-17 11:05:41 +0300

    Delete movie-tracker directory

--- movie-tracker/src/main.js
+++
@@ -1,9 +0,0 @@
-import { MovieModel }      from './model.js';
-import AppView             from './views/AppView.js';
-import { MoviePresenter }  from './presenter.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const model     = new MovieModel();
-  const view      = new AppView(document.body);
-  new MoviePresenter(model, view);
-});
