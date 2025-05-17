96a3f2da260c939d2931647f166cf039f2304b61
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-05-17 11:28:32 +0300

    Update AppView.js

--- src/views/AppView.js
+++ src/views/AppView.js
@@ -1,26 +1,27 @@
-import MovieFormView   from './MovieFormView.js';
+import MovieFormView from './MovieFormView.js';
 import MovieFilterView from './MovieFilterView.js';
-import MovieListView   from './MovieListView.js';
+import MovieListView from './MovieListView.js';
 import { render, RenderPosition } from '../framework/render.js';
 
 export default class AppView {
-  constructor(root) {
-    this.root       = root;
-    this.formView   = new MovieFormView();
+  constructor() {
+    this.formView = new MovieFormView();
     this.filterView = new MovieFilterView();
-    this.listView   = new MovieListView();
+    this.listView = new MovieListView();
   }
 
   init({ onAdd, onFilter, onEdit, onDelete }) {
-    const container = this.root.querySelector('.container');
+    const formContainer = document.querySelector('.movie-form');
+    const filterContainer = document.querySelector('.movie-filter');
+    const listContainer = document.querySelector('#movie-list');
 
-    render(this.formView,   container, RenderPosition.BEFOREEND);
-    render(this.filterView, container, RenderPosition.BEFOREEND);
-    render(this.listView,   container, RenderPosition.BEFOREEND);
+    render(this.formView, formContainer, RenderPosition.BEFOREEND);
+    render(this.filterView, filterContainer, RenderPosition.BEFOREEND);
+    render(this.listView, listContainer, RenderPosition.BEFOREEND);
 
     this.formView.bindAdd(onAdd);
     this.filterView.bindFilter(onFilter);
-    this._onEdit   = onEdit;
+    this._onEdit = onEdit;
     this._onDelete = onDelete;
   }
 
